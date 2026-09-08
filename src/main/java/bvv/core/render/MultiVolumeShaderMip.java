9daa1a09f3274f735546d2a8e4789c1e2a562b59
Eugene Katrukha <katpyxa@gmail.com>
2026-09-08 22:53:39 +0200

    modify sources test to show grid

--- src/main/java/bvv/core/render/MultiVolumeShaderMip.java
+++ src/main/java/bvv/core/render/MultiVolumeShaderMip.java
@@ -273,9 +273,9 @@ public class MultiVolumeShaderMip
 //		final StringBuilder vertexShaderCode = prog.getVertexShaderCode();
 //		System.out.println( "vertexShaderCode = " + vertexShaderCode );
 //		System.out.println( "\n\n--------------------------------\n\n" );
-		final StringBuilder fragmentShaderCode = prog.getFragmentShaderCode();
-		System.out.println( "fragmentShaderCode = " + fragmentShaderCode );
-		System.out.println( "\n\n--------------------------------\n\n" );
+//		final StringBuilder fragmentShaderCode = prog.getFragmentShaderCode();
+//		System.out.println( "fragmentShaderCode = " + fragmentShaderCode );
+//		System.out.println( "\n\n--------------------------------\n\n" );
 	}
 
 	public static Map< SegmentType, SegmentTemplate > getDefaultSegments( boolean useDepthTexture )
